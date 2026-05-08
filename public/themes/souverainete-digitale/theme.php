bc798dea7993ad27fa5088e714de8a738a28ede9
PolyGlotJerbi <khalil.jerbi@otospex.com>
2026-05-08 15:35:40 +0100

    trnaslte to english the theme

--- public/themes/souverainete-digitale/theme.php
+++ public/themes/souverainete-digitale/theme.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
-Name: Souveraineté Digitale
+Name: Digital Sovereignty
 Slug: souverainete-digitale
 URI: https://vvveb.com
 Author: Otospex
 Author URI: https://vvveb.com
-Description: Clean B2B landing page focused on Digital Sovereignty consultations.
+Description: Clean B2B landing page focused on digital sovereignty consultations.
 Version: 1.0
 License:  Apache 2.0
 License URI: https://vvveb.com/licence/
